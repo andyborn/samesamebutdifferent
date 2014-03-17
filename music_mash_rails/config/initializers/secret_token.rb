cac0f7c74d7ba35661b5b292ab37d2251b07bd96
Andy Born <andybornx@gmail.com>
2014-03-17 16:56:58 +0000

    new rails app setup

---
+++ music_mash_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicMashRails::Application.config.secret_token = '46c25330b5826828872fbee863556bd122c144948c20647498c6b95af2ca128668da0e809806d270a0de1d19fd52b34e391965a339396a8a644a2b7f5b7306cf'
