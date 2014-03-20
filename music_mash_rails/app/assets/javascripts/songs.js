93ec28bc49f68af4556b4336746579c6089cccc7
Andy Born <andybornx@gmail.com>
2014-03-20 21:00:35 +0000

    sends song names to underscore template in index page

--- music_mash_rails/app/assets/javascripts/songs.js
+++ music_mash_rails/app/assets/javascripts/songs.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
   // var songValues = {},
   // var songInfo = $('#songTemplate').html(),
 
-  
+  var parsedTemplate = "";
+  var tmpl_song = $('#tmpl_song').html();
+
+  var songTemplate = _.template(tmpl_song);
 
   $('#new_song').on('submit', function(ev){
-      ev.preventDefault();
 
+      ev.preventDefault();
+      $('#similar_songs_collection').html('');
       var artist_name = $('#song_artist_name').val();
       var song_name = $('#song_song_name').val();
       var deezer_url = $('#song_deezer_url').val();
@@ -22,14 +26,21 @@ $(document).ready(function() {
             url: '/songs/' + data.id + '.json',
             success: function(json) {
                           _.each(json, function(track) {
-                            console.log(track); 
+                            parsedTemplate += songTemplate(track); 
 
 
                             }); // close _.each
+                                console.log('before');
+                            console.log(parsedTemplate);
+                            console.log('after');
+                            $('#similar_songs_collection').html(parsedTemplate);
+                            parsedTemplate = "";
+
                     } // close success2
           }); // close AJAX2
         } // close success1
       }); //close AJAX1
+
   }); // close submit function
 
   
