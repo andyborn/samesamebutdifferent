5a760e22c7c2ff85adb389d02341657293cbc103
Andy Born <andybornx@gmail.com>
2014-03-23 12:23:45 +0000

    styling for main song page

--- music_mash_rails/app/assets/javascripts/songs.js
+++ music_mash_rails/app/assets/javascripts/songs.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+  // $.noty.defaults = {
+  //   layout: 'top',
+  //   theme: 'defaultTheme',
+  //   type: 'alert',
+  //   text: '', // can be html or string
+  //   dismissQueue: true, // If you want to use queue feature set this true
+  //   template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+  //   animation: {
+  //       open: {height: 'toggle'},
+  //       close: {height: 'toggle'},
+  //       easing: 'swing',
+  //       speed: 500 // opening & closing animation speed
+  //   },
+  //   timeout: 200, // delay for closing event. Set false for sticky notifications
+  //   force: false, // adds notification to the beginning of queue when set to true
+  //   modal: false,
+  //   maxVisible: 5, // you can set max visible notification for dismissQueue true option,
+  //   killer: false, // for close all notifications before show
+  //   closeWith: ['click'], // ['click', 'button', 'hover']
+  //   callback: {
+  //       onShow: function() {},
+  //       afterShow: function() {},
+  //       onClose: function() {},
+  //       afterClose: function() {}
+  //   },
+  //   buttons: false // an array of buttons
+  // };
+
   // var songValues = {},
   // var songInfo = $('#songTemplate').html(),
 
@@ -42,15 +70,35 @@ $(document).ready(function() {
                         $('#similar_songs_collection').html(parsedTemplate);
                         parsedTemplate = "";
 
+                        $('.song_remove').on('click', function(ev){ 
+                            $(ev.currentTarget).parents('.similar_song').hide();
+                        });
+
                         $('.deezer_send').on('click', function(ev){
                               
                             var deezer_id = $(ev.currentTarget).data('deezer-id');
                             $.ajax({
                                 url: '/deezer/post_to_deezer.json',
                                 method: 'POST',
-                                data: {'deezer_id':deezer_id}
-                                
-                                
+                                data: {'deezer_id':deezer_id},
+                                success: function(json) {
+                                    $(ev.currentTarget).hide();
+                                    
+                                    var songAdded = noty({
+                                      text: 'Song added to your Deezer profile!',
+                                      timeout: 2000,
+                                      type: 'success'
+                                    });
+                                  },
+                                error: function(json) {
+                                  // "Track already exists"
+                                  var n = noty({
+                                    text: json.responseJSON.error.message,
+                                    type: 'error',
+                                    timeout: 2000
+                                  });
+                                  
+                                }
 
                             }); //close AJAX3    
 
--- music_mash_rails/app/controllers/deezer_controller.rb
+++ music_mash_rails/app/controllers/deezer_controller.rb
@@ -6,18 +6,17 @@ class DeezerController < ApplicationController
       user_id = current_user.deezer_id
       session_token = current_user.token
       deezer_id = params[:deezer_id]
-      url= "https://api.deezer.com/user/#{user_id}/tracks?output=jsonp&request_method=POST&track_id=#{deezer_id}&output=jsonp&access_token=#{session_token}"
+      url= "https://api.deezer.com/user/#{user_id}/tracks?output=json&request_method=POST&track_id=#{deezer_id}&output=jsonp&access_token=#{session_token}"
       
       response = HTTParty.get(url)
       # response = HTTParty.get("https://api.deezer.com/user/448706601/tracks?output=jsonp&request_method=POST&track_id=74415139&output=jsonp&access_token=froSVuFpNj532c693306a26oGnUom4w532c693306a64Wrelkxj")
-      binding.pry
+      response = response[1..-2] # get rid of the brackets around the response... it should be JSON, but it's not... quite
+
       respond_to do |format|
-      if response = "(true)"
-        
-        format.json { render json: {}, status: :success }
+      if response == "true"        
+        format.json { render json: {}, status: :created }
       else
-        
-        format.json { render json: {}, status: :error }
+        format.json { render json: JSON.parse(response), status: :unprocessable_entity }
       end
     end
     end
