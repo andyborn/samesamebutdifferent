feca03bec794498de75d95c397e04599eafd4306
Andy Born <andybornx@gmail.com>
2014-03-24 20:03:49 +0000

    fix animations on data retrieval

--- music_mash_rails/app/assets/javascripts/songs.js
+++ music_mash_rails/app/assets/javascripts/songs.js
@@ -57,6 +57,8 @@ $(document).ready(function() {
         method: 'POST',
         data: {'song[artist_name]':artist_name, 'song[song_name]':song_name, 'song[deezer_url]':deezer_url},
         error: function(json) {
+              $('#cog1').removeClass('rotating1');
+              $('#cog2').removeClass('rotating2');
               $('#similar_songs_collection').html('<h2>Sorry, song not found</h2>').focus();
             },
         success: function(data) {
@@ -67,128 +69,125 @@ $(document).ready(function() {
             success: function(json) {
                 $('#cog1').removeClass('rotating1');
                 $('#cog2').removeClass('rotating2');
-                _.each(json, function(track) {
-                    
-                    if (track.image == null)
-                        {
+                
+                _.each(json, function(track) {                    
+                    if (track.image == null){
                             var defaultCover = "http://www.rocksound.tv/images/uploads/deezer300.jpg"
                             track.image = {}
                             track.image = track.image = [{"#text":defaultCover,"size":"small"},{"#text":defaultCover,"size":"medium"},{"#text":defaultCover,"size":"large"},{"#text":defaultCover,"size":"extralarge"}];
-                            
-                        }
+                      }
 
-                    if (track.deezer != null) 
-                      {
-                        parsedTemplate += songTemplate(track);
-                      }  // close if
-                    else 
-                        {
+                    if (track.deezer != null) {
+                          parsedTemplate += songTemplate(track);
+                      } else {
                           track.deezer = {
                           link: "deezer track not found",
                           preview: "deezer track not found"
-                              }; // close attributes.
-                        }   // close else.     
-                         
-                    
-                     
-
-
+                          }; // close attributes.
+                      }   // close if.    
                 }); // close _.each
+                
+                
+                    
                     if (parsedTemplate != "") {
-                        $('#similar_songs_collection').html('<h1>Side B</h1>' + parsedTemplate);
-                        parsedTemplate = "";
+                        $('#similar_songs_collection').fadeOut(function(){
+                          $('#similar_songs_collection').html('<h1>Side B</h1>' + parsedTemplate);
+                          parsedTemplate = "";
+                          $('#similar_songs_collection').fadeIn();
+                          
+                      
 
-                        }
-                    else 
-                        {  
-                          $('#similar_songs_collection').html('<h2>Sorry, no Deezer data returned for this song.</h2><h2>Try checking that artist name is spelt correctly (eg. The Rolling Stones, not Rolling Stones!) </h2>');
-                        }
-
-                        $('.similar_song').mouseenter(function(){
-                          $(this).find('.play_icon').show(100);
-                          $(this).find('.song_dropdown').show(100);
-                        });
-
-                        $('.similar_song').mouseleave(function(){
-                          $(this).find('.play_icon').hide(100);
-                          $(this).find('.song_dropdown').hide(100);
-                        });
-
-                        $('.play_icon').click(function(){
-                          var that = $(this).parent('.similar_song').find('.song_player').get(0);
-                          if (that.paused == false) {
-                                that.pause();
-                                
-                            } else {
-                                that.play();
-                                
-                            }
+                          $('.similar_song').mouseenter(function(){
+                            $(this).find('.play_icon').show(100);
+                            $(this).find('.song_dropdown').show(100);
                           });
 
-                          // $(this).parent('.similar_song').find('.song_player').trigger("play");
-                          
+                          $('.similar_song').mouseleave(function(){
+                            $(this).find('.play_icon').hide(100);
+                            $(this).find('.song_dropdown').hide(100);
+                          });
 
-                        $('.song_info').click(function(){
-                          $(this).parent('.similar_song').find('.song_dropdown').slideToggle();
-                        });
+                          $('.play_icon').click(function(){
+                            var that = $(this).parent('.similar_song').find('.song_player').get(0);
+                            if (that.paused == false) {
+                                  that.pause();
+                                  
+                              } else {
+                                  that.play();
+                                  
+                              }
+                          });
 
-                        $('.song_info').click(function(){
-                          $(this).parent('.similar_song').find('.song_dropdown').slideToggle();
-                        });
+                            // $(this).parent('.similar_song').find('.song_player').trigger("play");
+                            
 
-                        $('.song_remove').on('click', function(ev){ 
-                            $(ev.currentTarget).parents('.similar_song').remove();
-                        });
+                          $('.song_info').click(function(){
+                            $(this).parent('.similar_song').find('.song_dropdown').slideToggle();
+                          });
 
-                        $('.deezer_more').on('click', function(ev){ 
-                            var deezer_url = $(ev.currentTarget).data('deezer-url');
-                            var song_name = $(ev.currentTarget).data('song-name');
-                            var artist_name = $(ev.currentTarget).data('artist-name');
+                          $('.song_info').click(function(){
+                            $(this).parent('.similar_song').find('.song_dropdown').slideToggle();
+                          });
 
-                            $('#song_artist_name').val(artist_name);
-                            $('#song_song_name').val(song_name);
-                            getSongs(ev, deezer_url);
-                        });
+                          $('.song_remove').on('click', function(ev){ 
+                              $(ev.currentTarget).parents('.similar_song').remove();
+                          });
 
-                        $('.deezer_send').on('click', function(ev){
-                              
-                            var deezer_id = $(ev.currentTarget).data('deezer-id');
-                            $.ajax({
-                                url: '/deezer/post_to_deezer.json',
-                                method: 'POST',
-                                data: {'deezer_id':deezer_id},
-                                success: function(json) {
-                                    $(ev.currentTarget).hide();
-                                    
-                                    var songAdded = noty({
-                                      text: 'Song added to your Deezer profile!',
-                                      timeout: 2000,
-                                      type: 'success'
-                                    });
-                                  },
-                                error: function(json) {
-                                    // "Track already exists"
-                                    if (json.responseJSON.error.message == "Track already exists")
-                                        { var n = noty({
-                                          text: 'You have already added this track to you deezer profile',
+                          $('.deezer_more').on('click', function(ev){ 
+                              var deezer_url = $(ev.currentTarget).data('deezer-url');
+                              var song_name = $(ev.currentTarget).data('song-name');
+                              var artist_name = $(ev.currentTarget).data('artist-name');
+
+                              $('#song_artist_name').val(artist_name);
+                              $('#song_song_name').val(song_name);
+                              getSongs(ev, deezer_url);
+                          });
+
+                          $('.deezer_send').on('click', function(ev){
+                                
+                              var deezer_id = $(ev.currentTarget).data('deezer-id');
+                              $.ajax({
+                                  url: '/deezer/post_to_deezer.json',
+                                  method: 'POST',
+                                  data: {'deezer_id':deezer_id},
+                                  success: function(json) {
+                                      $(ev.currentTarget).hide();
+                                      
+                                      var songAdded = noty({
+                                        text: 'Song added to your Deezer profile!',
+                                        timeout: 2000,
+                                        type: 'success'
+                                      });
+                                    },
+                                  error: function(json) {
+                                      // "Track already exists"
+                                      if (json.responseJSON.error.message == "Track already exists")
+                                          { var n = noty({
+                                            text: 'You have already added this track to you deezer profile',
+                                            type: 'error',
+                                            timeout: 2000}); // close noty
+                                            $(ev.currentTarget).hide();
+                                          }
+
+                                      else {  
+                                      var n = noty({
+                                          text: 'Deezer session token has expired, please log in again to refresh',
                                           type: 'error',
-                                          timeout: 2000}); // close noty
-                                          $(ev.currentTarget).hide();
-                                        }
+                                          timeout: 2000
+                                          }); // close noty
+                                      }
+                                  }
 
-                                    else {  
-                                    var n = noty({
-                                        text: 'Deezer session token has expired, please log in again to refresh',
-                                        type: 'error',
-                                        timeout: 2000
-                                        }); // close noty
-                                    }
-                                }
+                              }); //close AJAX3    
 
-                            }); //close AJAX3    
-
-                        });// close button event
-
+                          });// close button event
+                        });//close fadeOut callback
+                      } else { 
+                          $('#similar_songs_collection').fadeOut(function(){ 
+                          $('#similar_songs_collection').html('<h2>Sorry, no Deezer data returned for this song.</h2><h2>Try checking that artist name is spelt correctly (eg. The Rolling Stones, not Rolling Stones!) </h2>');
+                          $('#similar_songs_collection').fadeIn();
+                        }); // close fadeOut callback
+                      } // close if data empty loop
                     } // close success2
           }); // close AJAX2
         } // close success1
